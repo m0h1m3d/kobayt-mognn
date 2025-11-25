1b582cbb4bddb9540e4102f44cbb9ba22dad8e5c
m0h1m3d <mohisham33@gmail.com>
2025-11-25 00:26:12 -0800

    refractor

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,12 +5,12 @@ function Button({ desc, className }) {
   const btnRef = useRef(null);
 
   function handleBtnClick() {
-    const button = btnRef.current;
-    button.style.transform = "scale(0.8)";
+    // const button = btnRef.current;
+    // button.style.transform = "scale(0.8)";
 
-    setTimeout(() => {
-      button.style.transform = "scale(1)";
-    }, 200);
+    // setTimeout(() => {
+    //   button.style.transform = "scale(1)";
+    // }, 200);
   }
 
   return (
