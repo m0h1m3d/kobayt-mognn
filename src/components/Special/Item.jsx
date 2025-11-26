ffff78dcfcdac43bb4d635d01c82f8293b220247
m0h1m3d <mohisham33@gmail.com>
2025-11-25 19:02:23 -0800

    add calling feature

--- src/components/Special/Item.jsx
+++ src/components/Special/Item.jsx
@@ -1,23 +1,27 @@
 import { handleBtnClick } from "../../helper/buttonEffect";
 
-function Item({img,name,desc,price, newClass}) {
-    return (
-        <div className={`item ${newClass}`}>
-            <img src={img} alt={name} />
-            <h3>{name}</h3>
-            <p>{desc}</p>
-            <span className="price">
-                ${price}
-                <Button onClick={e => handleBtnClick(e)}/>
-                </span>
-        </div>
-    )
+function Item({ img, name, desc, price, newClass }) {
+  return (
+    <div className={`item ${newClass}`}>
+      <img src={img} alt={name} />
+      <h3>{name}</h3>
+      <p>{desc}</p>
+      <span className="price">
+        ${price}
+        <Button onClick={(e) => handleBtnClick(e)} />
+      </span>
+    </div>
+  );
 }
 
-function Button({onClick}){
-  return <button onClick={onClick} className={`item-btn`}>
-    Order Now
-  </button>
+function Button({ onClick }) {
+  return (
+    <a href="tel:0900007609">
+      <button onClick={onClick} className={`item-btn`}>
+        Call Now
+      </button>
+    </a>
+  );
 }
 
-export default Item
+export default Item;
