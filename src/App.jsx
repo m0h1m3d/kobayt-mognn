6ab950cdd01db16091e7fc020c519bfa1df0184e
m0h1m3d <mohisham33@gmail.com>
2025-12-02 00:17:48 -0800

    reset fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import "./App.css";
 import Features from "./components/Features/Features";
 import FollowUs from "./components/FollowUS/FollowUs";
@@ -7,13 +6,6 @@ import HeaderHero from "./components/HeaderHero/HeaderHero";
 import Special from "./components/Special/Special";
 
 function App() {
-  useEffect(() => {
-    const ua = navigator.userAgent || navigator.vendor || window.opera;
-    if (ua.includes("Instagram")) {
-      window.location.href =
-        "https://kobayt-mognn.vercel.app/?openExternalBrowser=1";
-    }
-  }, []);
   return (
     <>
       <HeaderHero />
